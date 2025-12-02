20406051c410541841b2f5079ab1743b9513b828
Binay2410 <mailtobinaytripathy@gmail.com>
2025-12-02 13:27:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-from utils.comparator import compute_differences
-from utils.similarity import compute_similarity
-from utils.persistence import load_persistent_files
+from utils.comparator import run_full_comparison
+from utils.similarity import compute_similarity_summary
+from utils.persistence import load_history
 
 st.set_page_config(
     page_title="Security Group Analysis",
